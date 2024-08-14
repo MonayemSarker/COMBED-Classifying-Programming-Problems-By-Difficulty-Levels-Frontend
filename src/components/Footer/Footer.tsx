1ac7453a77fc6518006202f3a090157a1a4635f1
Monayem_Sarker <75083786+MonayemSarker@users.noreply.github.com>
2024-08-14 19:57:32 +0600

    log in page done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,11 @@
-import './Footer.css';
 
 function Footer() {
     return (
-        <footer>
-            <p>© 2024 COMBED</p>
+        <footer className="bg-white shadow  dark:bg-gray-800">
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="#" className="hover:underline">COMBED™</a>. All Rights Reserved.
+                </span>
+            </div>
         </footer>
     );
 }
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Login.css';
+
 
 interface LoginProps {
     onLogin: (username: string, password: string) => void;
@@ -15,27 +15,30 @@ function Login({ onLogin }: LoginProps) {
     };
 
     return (
-        <div className='container'>
-            <form onSubmit={handleSubmit} className="login-form">
-                <div>
-                    <label htmlFor="username">Username:</label>
+        <div className='h-[82vh] flex justify-center items-center'>
+            <form onSubmit={handleSubmit} className="max-w-sm mx-auto flex-col ">
+                <div className='mb-5'>
+                    <label htmlFor="username" className='block mb-2 text-sm font-medium text-gray-900'>Username:</label>
                     <input
                         type="text"
                         id="username"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
+                        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 '
                     />
                 </div>
-                <div>
+                <div className='mb-5'>
                     <label htmlFor="password">Password:</label>
                     <input
                         type="password"
                         id="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
+                        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 '
                     />
                 </div>
-                <button type="submit">Login</button>
+
+                <button type="submit" className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 self-center">Login</button>
             </form>
         </div>
     );
