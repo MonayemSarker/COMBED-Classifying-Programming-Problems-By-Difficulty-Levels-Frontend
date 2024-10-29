96d5e45ecae5005edd84dda11c026a4edba70084
MonayemSarker <bsse1228@iit.du.ac.bd>
2024-10-29 15:04:02 +0600

    ProtectedRoute implemented

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,13 +1,55 @@
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
+import { decodeToken, isTokenExpired } from "../../utils/authUtil";
 
 function ProtectedRoute({ children }: any) {
-  const accessToken = localStorage.getItem("accessToken");
+  const [isValidUser, setIsValidUser] = useState(true);
 
-  if (!accessToken) {
-    return <Navigate to="/" />;
-  }
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    console.log(isValidUser);
 
-  return children;
+    if (!accessToken) {
+      setIsValidUser(false);
+      return;
+    }
+
+    const decodedToken = decodeToken(accessToken);
+    // console.log(decodedToken);
+
+    if (!decodedToken || isTokenExpired(decodedToken.exp)) {
+      localStorage.removeItem("accessToken"); // Remove expired token
+      setIsValidUser(false);
+      return;
+    }
+    const validateUser = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/users/${decodedToken.sub}`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+
+        if (!response.ok) {
+          localStorage.removeItem("accessToken"); // Remove expired token
+          setIsValidUser(false);
+          throw new Error("User validation failed");
+        }
+        setIsValidUser(true);
+      } catch (error) {
+        console.error("User validation error:", error);
+        localStorage.removeItem("accessToken"); // Clean up if validation fails
+        setIsValidUser(false);
+      }
+    };
+
+    validateUser();
+  }, [localStorage.getItem("accessToken")]);
+
+  return isValidUser ? children : <Navigate to="/" />;
 }
 
 export default ProtectedRoute;
